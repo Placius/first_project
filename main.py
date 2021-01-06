4fea521187169a9a09669581f8a81fe180030007
Krystian Płatek <krystekpol@tlen.pl>
2021-01-06 16:30:57 +0100

    make win number & def for getting number from user

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # My first steps with learning Git and Github
 # Find my number game
+
+# import modules
+
+import random
+
+# drawing the winning number (1 - 100)
+
+win_number = int(random.randint(1,100))
+
+# creating a definition to getting number from user
+
+def GetNumber():
+    user_choice_number = int(input("Make your choice: "))
+    return user_choice_number
+
+# creating a game course

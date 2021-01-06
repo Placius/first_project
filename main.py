0ad23ef8ef0efd636843842d265d4c55ddd81849
Krystian Płatek <krystekpol@tlen.pl>
2021-01-06 17:07:42 +0100

    added win comment and numerating tries

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 # import modules
 
-import random
+import random, os, time
 
 # drawing the winning number (1 - 100)
 
@@ -19,8 +19,12 @@ def GetNumber():
 
 user_choice = None
 
+tries = 1
+
 while user_choice != win_number:
+    os.system("cls")
     try:
+        print("Try nr", tries, "\n\n")
         user_choice = GetNumber()
         # possibilities of choices
         if user_choice == win_number:
@@ -30,10 +34,14 @@ while user_choice != win_number:
             print("My number is only in ranges from 1 to 100...")
         
         elif user_choice < win_number:
+            os.system("cls")
             print("My number is higher...")
+            tries += 1
         
         elif user_choice > win_number:
+            os.system("cls")
             print("My number is lower...")
+            tries += 1
 
         else:
             print("Something goes wrong....")
@@ -42,4 +50,12 @@ while user_choice != win_number:
         print("You must choose number from 1 to 100! TRY AGAIN!")
         continue
 
+    time.sleep(2)
+
 # End of the game and congratulations
+
+os.system("cls")
+
+print("Congratulations! \n\n\nMy number is", str(win_number) + "!","\n\nYou'll make it in", str(tries),"tries!", "\n\nSee you soon and have a nice day!")
+    
+time.sleep(4)

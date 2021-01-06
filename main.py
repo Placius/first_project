7e1def5cdd25aaa7d9019b4283b4945e5cf39218
Krystian Płatek <krystekpol@tlen.pl>
2021-01-06 16:48:49 +0100

    Creating a game course

--- main.py
+++ main.py
@@ -16,3 +16,30 @@ def GetNumber():
     return user_choice_number
 
 # creating a game course
+
+user_choice = None
+
+while user_choice != win_number:
+    try:
+        user_choice = GetNumber()
+        # possibilities of choices
+        if user_choice == win_number:
+            break
+
+        elif user_choice > 100 or user_choice < 1:
+            print("My number is only in ranges from 1 to 100...")
+        
+        elif user_choice < win_number:
+            print("My number is higher...")
+        
+        elif user_choice > win_number:
+            print("My number is lower...")
+
+        else:
+            print("Something goes wrong....")
+    
+    except ValueError:
+        print("You must choose number from 1 to 100! TRY AGAIN!")
+        continue
+
+# End of the game and congratulations
